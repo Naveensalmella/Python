1bdc34cb553b27f0ede5b1d771862a9feb711b9b
Naveensalmella <naveensalmella46@gmail.com>
2026-02-05 16:18:48 +0530

    Day03.py

--- Day03.py
+++ Day03.py
@@ -34,4 +34,57 @@ n = int(input("Enter a number : "))
 for i in range(1, 11):
     print(n, "x", i, "=", n * i)
 
+#function 
+
+def greet():
+    print("Hello, Welcome")
+greet()
+
+#function with parameters
+
+def greet_user(name):
+    print("Hello", name)
+greet_user("Naveen Salmella")
+
+#function with return value
+#addition
+
+def add(a, b):
+    return a + b
+result = add(3, 5)
+print(result)
+
+#subtraction
+
+def sub(a, b):
+    return a - b
+result = sub(2, 5)
+print(result)
+
+#multiplication
+
+def mult(a, b):
+    return a * b
+result = mult(2, 7)
+print(result)
+
+#division
+
+def div(a, b):
+    return a / b
+result = div(10, 5)
+print(result)
+
+#create and access list
+
+fruits = ["Banana", "Apple", "Pomegranate"]
+print(fruits[0])
+print(len(fruits)) 
+
+#loop through list
+
+for fruit in fruits:
+    print(fruit)
+
+
 print("Day 3 is successfully completed")
