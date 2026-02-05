797625776c139a16b2ffa540fa27db4de4cf64d4
Naveensalmella <naveensalmella46@gmail.com>
2026-02-05 09:39:32 +0530

    Day02.py

--- Day02.py
+++ Day02.py
@@ -1,5 +1,29 @@
+#conditional statements in Python
+
 age = int(input("Enter your age : "))
 if age >= 18:
     print("You are a major")
 else:
     print("You are a minor")
+
+
+name = input("Enter your name : ")
+if name == "Naveen":
+    print("You born on 1st")
+elif name == "Srivally":
+    print("You are born on 26th")
+elif name == "Ramana Chary":
+    print("Tou born on 2nd")
+elif name == "Aruna":
+    print("You born on 30th")
+else:
+    print("You are not a family member")
+    
+
+num = int(input("Enter a number : "))
+if num > 0:
+    print("The number is POsitive")
+elif num < 0:
+    print("The number is Negative")
+else:
+    print("The number is Zero")
